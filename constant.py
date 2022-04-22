e7afd2d5a18002cc540cc480700f3ea04ce2fc93
CarlosDJY <103446832+CarlosDJY@users.noreply.github.com>
2022-04-22 14:38:51 +0800

    Update constant.py

--- constant.py
+++ constant.py
@@ -26,64 +26,77 @@ POSTEven_API = 'https://app.bupt.edu.cn/xisuncov/wap/open-report/save'
 # 当今日没有填报时，在https://app.bupt.edu.cn/ncov/wap/default/index下进行填报，
 # 全部填完，不要提交，f12打开控制台，在Console页面下输入代码 console.log(vm.info) 就会得到以下信息，之后每天就默认填以下信息
 INFO = r"""{
-        "address":"北京市海淀区北太平庄街道北京邮电大学计算机学院北京邮电大学海淀校区",
-        "area":"北京市  海淀区",
-        "bztcyy":"",
-        "city":"北京市",
-        "csmjry":"0",
-        "fjqszgjdq":"",
-        "geo_api_info":"{\"type\":\"complete\",\"position\":{\"Q\":39.960390625,\"R\":116.356397569445,\"lng\":116.356398,\"lat\":39.960391},\"location_type\":\"html5\",\"message\":\"Get ipLocation failed.Get geolocation success.Convert Success.Get address success.\",\"accuracy\":23,\"isConverted\":true,\"status\":1,\"addressComponent\":{\"citycode\":\"010\",\"adcode\":\"110108\",\"businessAreas\":[{\"name\":\"北下关\",\"id\":\"110108\",\"location\":{\"Q\":39.955976,\"R\":116.33873,\"lng\":116.33873,\"lat\":39.955976}},{\"name\":\"西直门\",\"id\":\"110102\",\"location\":{\"Q\":39.942856,\"R\":116.34666099999998,\"lng\":116.346661,\"lat\":39.942856}},{\"name\":\"小西天\",\"id\":\"110108\",\"location\":{\"Q\":39.957147,\"R\":116.364058,\"lng\":116.364058,\"lat\":39.957147}}],\"neighborhoodType\":\"科教文化服务;学校;高等院校\",\"neighborhood\":\"北京邮电大学\",\"building\":\"北京邮电大学计算机学院\",\"buildingType\":\"科教文化服务;学校;高等院校\",\"street\":\"西土城路\",\"streetNumber\":\"10号\",\"country\":\"中国\",\"province\":\"北京市\",\"city\":\"\",\"district\":\"海淀区\",\"township\":\"北太平庄街道\"},\"formattedAddress\":\"北京市海淀区北太平庄街道北京邮电大学计算机学院北京邮电大学海淀校区\",\"roads\":[],\"crosses\":[],\"pois\":[],\"info\":\"SUCCESS\"}",
-        "glksrq":"",
-        "gllx":"",
-        "gtjzzchdfh":"",
-        "gtjzzfjsj":"",
-        "ismoved":"0",
-        "jcbhlx":"",
-        "jcbhrq":"",
-        "jchbryfs":"",
-        "jcjgqr":"0",
-        "jcwhryfs":"",
-        "jhfjhbcc":"",
-        "jhfjjtgj":"",
-        "jhfjrq":"",
-        "mjry":"0",
-        "province":"北京市",
-        "qksm":"",
-        "remark":"",
-        "sfcxtz":"0",
-        "sfcxzysx":"0",
-        "sfcyglq":"0",
-        "sfjcbh":"0",
-        "sfjchbry":"0",
-        "sfjcwhry":"0",
-        "sfjzdezxgym":"1",
-        "sfjzxgym":"1",
-        "sfsfbh":"0",
-        "sftjhb":"0",
-        "sftjwh":"0",
-        "sfxk":"0",
-        "sfygtjzzfj":"",
-        "sfyyjc":"0",
-        "sfzx":1,
-        "szcs":"",
-        "szgj":"",
-        "szsqsfybl":"0",
-        "tw":"2",
-        "xjzd":"",
-        "xkqq":"",
-        "xwxgymjzqk":"3",
-        "ymjzxgqk":"已接种",
-        "zgfxdq":"0"
+        address: "上海市宝山区张庙街道南北高架路宝山购物中心"
+        area: "上海市  宝山区"
+        bztcyy: ""
+        city: "上海市"
+        created: 1650585636
+        created_uid: 0
+        csmjry: "0"
+        date: "20220422"
+        fxyy: ""
+        geo_api_info: "{\"type\":\"complete\",\"position\":{\"Q\":31.325374077691,\"R\":121.44423611111199,\"lng\":121.444236,\"lat\":31.325374},\"location_type\":\"html5\",\"message\":\"Get ipLocation failed.Get geolocation success.Convert Success.Get address success.\",\"accuracy\":94,\"isConverted\":true,\"status\":1,\"addressComponent\":{\"citycode\":\"021\",\"adcode\":\"310113\",\"businessAreas\":[{\"name\":\"共康\",\"id\":\"310113\",\"location\":{\"Q\":31.320874,\"R\":121.43926299999998,\"lng\":121.439263,\"lat\":31.320874}},{\"name\":\"庙行镇\",\"id\":\"310113\",\"location\":{\"Q\":31.329455,\"R\":121.43615599999998,\"lng\":121.436156,\"lat\":31.329455}},{\"name\":\"通河\",\"id\":\"310113\",\"location\":{\"Q\":31.333801,\"R\":121.44710800000001,\"lng\":121.447108,\"lat\":31.333801}}],\"neighborhoodType\":\"\",\"neighborhood\":\"\",\"building\":\"\",\"buildingType\":\"\",\"street\":\"共和新路\",\"streetNumber\":\"5199甲\",\"country\":\"中国\",\"province\":\"上海市\",\"city\":\"\",\"district\":\"宝山区\",\"towncode\":\"310113008000\",\"township\":\"张庙街道\"},\"formattedAddress\":\"上海市宝山区张庙街道南北高架路宝山购物中心\",\"roads\":[],\"crosses\":[],\"pois\":[],\"info\":\"SUCCESS\"}"
+        glksrq: ""
+        gllx: ""
+        gtjzzfjsj: ""
+        gwszdd: ""
+        id: 19786025
+        ismoved: 0
+        jcbhlx: ""
+        jcbhrq: ""
+        jchbryfs: ""
+        jcjg: ""
+        jcjgqr: "0"
+        jcqzrq: ""
+        jcwhryfs: ""
+        jhfjhbcc: ""
+        jhfjjtgj: ""
+        jhfjrq: ""
+        jrsfqzfy: ""
+        jrsfqzys: ""
+        mjry: "0"
+        province: "上海市"
+        qksm: ""
+        remark: ""
+        sfcxtz: "0"
+        sfcxzysx: "0"
+        sfcyglq: "0"
+        sfjcbh: "0"
+        sfjchbry: "0"
+        sfjcqz: ""
+        sfjcwhry: "0"
+        sfjzdezxgym: 0
+        sfjzxgym: 0
+        sfsfbh: 0
+        sfsqhzjkk: 0
+        sftjhb: "0"
+        sftjwh: "0"
+        sfxk: 0
+        sfygtjzzfj: 0
+        sfyqjzgc: ""
+        sfyyjc: "0"
+        sfzx: 0
+        sqhzjkkys: ""
+        szcs: ""
+        szgj: ""
+        szsqsfybl: "1"
+        tw: "3"
+        uid: "83798"
+        xjzd: ""
+        xkqq: ""
+        xwxgymjzqk: 3
+        ymjzxgqk: "无"
+        zgfxdq: "0"
         }"""
 
 INFO_E = r"""{
     "sfzx": "0",
-    "tw":"1",
-    "area":"北京市  海淀区",
-    "city":"北京市",
-    "province":"北京市",
-    "address":"北京市海淀区北太平庄街道北京邮电大学计算机学院北京邮电大学海淀校区",
-    "geo_api_info": "{\"type\":\"complete\",\"info\":\"SUCCESS\",\"status\":1,\"fEa\":\"jsonp_940261_\",\"position\":{\"Q\":39.960,\"R\":116.35640,\"lng\":116.356340,\"lat\":39.960},\"message\":\"Get geolocation time out.Get ipLocation success.Get address success.\",\"location_type\":\"ip\",\"accuracy\":null,\"isConverted\":true,\"addressComponent\":{\"citycode\":\"010\",\"adcode\":\"100876\",\"businessAreas\":[],\"neighborhoodType\":\"科教文化服务;学校;高等院校\",\"neighborhood\":\"北京邮电大学\",\"building\":\"北京邮电大学计算机学院\",\"buildingType\":\"科教文化服务;学校;高等院校\",\"street\":\"西土城路\",\"streetNumber\":\"10号\",\"country\":\"中国\",\"province\":\"北京市\",\"city\":\"\",\"district\":\"海淀区\",\"towncode\":\"\",\"township\":\"北太平庄街道\"},\"formattedAddress\":\"北京市海淀区北太平庄街道北京邮电大学计算机学院北京邮电大学海淀校区\",\"roads\":[],\"crosses\":[],\"pois\":[]}",
+    "tw":"3",
+    "area":"上海市  宝山区",
+    "city":"上海市",
+    "province":"上海市",
+    "address":"上海市宝山区张庙街道南北高架路宝山购物中心",
+    "geo_api_info": "{\"type\":\"complete\",\"position\":{\"Q\":31.325374077691,\"R\":121.44423611111199,\"lng\":121.444236,\"lat\":31.325374},\"location_type\":\"html5\",\"message\":\"Get ipLocation failed.Get geolocation success.Convert Success.Get address success.\",\"accuracy\":94,\"isConverted\":true,\"status\":1,\"addressComponent\":{\"citycode\":\"021\",\"adcode\":\"310113\",\"businessAreas\":[{\"name\":\"共康\",\"id\":\"310113\",\"location\":{\"Q\":31.320874,\"R\":121.43926299999998,\"lng\":121.439263,\"lat\":31.320874}},{\"name\":\"庙行镇\",\"id\":\"310113\",\"location\":{\"Q\":31.329455,\"R\":121.43615599999998,\"lng\":121.436156,\"lat\":31.329455}},{\"name\":\"通河\",\"id\":\"310113\",\"location\":{\"Q\":31.333801,\"R\":121.44710800000001,\"lng\":121.447108,\"lat\":31.333801}}],\"neighborhoodType\":\"\",\"neighborhood\":\"\",\"building\":\"\",\"buildingType\":\"\",\"street\":\"共和新路\",\"streetNumber\":\"5199甲\",\"country\":\"中国\",\"province\":\"上海市\",\"city\":\"\",\"district\":\"宝山区\",\"towncode\":\"310113008000\",\"township\":\"张庙街道\"},\"formattedAddress\":\"上海市宝山区张庙街道南北高架路宝山购物中心\",\"roads\":[],\"crosses\":[],\"pois\":[],\"info\":\"SUCCESS\"}",
     "sfcyglq": "0",
     "sfyzz": "0","qtqk": "","askforleave": "0"
     }"""
